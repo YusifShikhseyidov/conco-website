d67edd6b668ff926a12449d2659f444484b3c9cc
Yusif Shikhseyidov <yusif1580@gmail.com>
2024-02-01 22:56:42 +0400

    backend-api folder deleted from frontend folder

--- backend-api/src/api/vacancy/routes/vacancy.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * vacancy router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::vacancy.vacancy');
--- backend-api/src/api/vacancy/services/vacancy.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * vacancy service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::vacancy.vacancy');
