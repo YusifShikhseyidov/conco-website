3ea396252fd23dedcbd5f9155ffc64a774884ccc
Yusif Shikhseyidov <yusif1580@gmail.com>
2024-02-07 15:38:27 +0400

    removed "showMoreBtn" component from "singleProject" component

--- src/components/SingleProjectComponent/SingleProject.jsx
+++ src/components/SingleProjectComponent/SingleProject.jsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
 import { Fade } from "react-awesome-reveal";
-import ShowMoreBtn from "../ShowMoreBtn/ShowMoreBtn";
 
 import "./SingleProject.css";
 
 export default function SingleProject({photos, title}) {
-  const [toggle, setToggle] = useState(false);
 
   return (
     <>
@@ -15,8 +12,6 @@ export default function SingleProject({photos, title}) {
             <p className="single-project_text-wrapper">
               {title}
             </p>
-            {/* this is a test */}
-            <ShowMoreBtn toggle={toggle} setToggle={setToggle} />
           </div>
         </Fade>
 
@@ -30,28 +25,7 @@ export default function SingleProject({photos, title}) {
           </ul>
         </Fade>
       </section>
-      {/* this is a test */}
-      {toggle && (
-        <p className="single-project_extra-content-box">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis,
-          deserunt cum vel veniam omnis quam soluta, hic aliquid atque saepe
-          voluptatibus optio harum magni ullam nihil dolores praesentium, vitae
-          repellat! Ducimus veritatis, ratione consectetur architecto molestiae
-          doloremque, totam officiis, rem sit reprehenderit quisquam. Ex numquam
-          voluptatem quaerat minima, voluptatum quod esse facere molestias
-          consequatur. Accusamus eaque illo maxime obcaecati explicabo. Tempore
-          dolores accusamus voluptate quam quia officiis animi tenetur cumque
-          harum eveniet facilis omnis aliquid possimus, assumenda cupiditate
-          blanditiis nihil numquam, non atque voluptas! Facere qui quas nemo
-          laborum officiis. Tempora officiis beatae rerum tenetur quasi nobis
-          itaque tempore est rem modi, quia, dolor ad suscipit ipsum quis fugiat
-          repellat perspiciatis porro totam eos sint vitae eligendi voluptate.
-          Aliquid, neque! Ipsum expedita corporis officiis ullam cumque minima
-          adipisci alias, voluptate dolorum accusamus debitis laudantium ipsam
-          id excepturi mollitia nostrum quibusdam esse error voluptates
-          repellendus veritatis facilis quod sunt aliquam! Accusamus?
-        </p>
-      )}
+      
     </>
   );
 }
