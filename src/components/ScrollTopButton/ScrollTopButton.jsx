8faae47ed2791b386def4e6a4c5bba5a222dfbcb
Yusif Shikhseyidov <yusif1580@gmail.com>
2024-04-28 05:07:37 +0400

    scrolltop btn changed to have smoother scroll

--- src/components/ScrollTopButton/ScrollTopButton.jsx
+++ src/components/ScrollTopButton/ScrollTopButton.jsx
@@ -1,35 +1,58 @@
-import { useEffect, useState } from 'react';
-import './ScrollTopButton.css'
+import { useEffect, useState } from "react";
+import "./ScrollTopButton.css";
 import { SlArrowUp } from "react-icons/sl";
 
 export default function ScrollTopButton() {
+  const [showButton, setShowButton] = useState(false);
 
-  const [showButton, setShowButton] = useState(false)
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 400) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    });
+  }, []);
 
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior:'smooth'
-    })
+  const TIMINGFUNC_MAP = {
+    linear: (t) => t,
+    "ease-in": (t) => t * t,
+    "ease-out": (t) => t * (2 - t),
+    "ease-in-out": (t) => (t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t),
+  };
+
+  function scrollTopSmooth(initY, duration = 300, timingName = "linear") {
+    const timingFunc = TIMINGFUNC_MAP[timingName];
+    let start = null;
+    const step = (timestamp) => {
+      start = start || timestamp;
+      const progress = timestamp - start,
+        // Growing from 0 to 1
+        time = Math.min(1, (timestamp - start) / duration);
+
+      window.scrollTo(0, initY - timingFunc(time) * initY);
+      if (progress < duration) {
+        window.requestAnimationFrame(step);
+      }
+    };
+
+    window.requestAnimationFrame(step);
   }
 
-
-
-  useEffect(()=>{
-    window.addEventListener('scroll',()=>{
-      if(window.scrollY>400){
-        setShowButton(true)
-      }else{
-        setShowButton(false)
-      }
-    })
-  }, [])
+  // Subscribe any element with ".scrollup-container"
+  Array.from(document.querySelectorAll(".scrollup-container")).forEach(
+    (btn) => {
+      btn.addEventListener("click", (e) => {
+        e.preventDefault();
+        scrollTopSmooth(window.scrollY, 2000, "ease-in-out");
+      });
+    }
+  );
 
   return (
-    <div className={`scrollup-container ${showButton ? 'show' : ''}`} onClick={scrollToTop}>
-      {showButton && (
-        <SlArrowUp className='arrow-icon' />
-      )}
+    <div className={`scrollup-container ${showButton ? "show" : ""}`}>
+      {showButton && <SlArrowUp className="arrow-icon" />}
     </div>
-  )
+  );
 }
