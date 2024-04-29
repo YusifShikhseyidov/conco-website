bd891f83d6e27703e403a180f5b424cf649dca9e
Yusif Shikhseyidov <yusif1580@gmail.com>
2024-04-29 20:08:50 +0400

    I removed language detector from i18n file

--- src/i18n.js
+++ src/i18n.js
@@ -1,9 +1,8 @@
 import i18n from "i18next"
 import Backend from "i18next-http-backend"
-import LanguageDetector from "i18next-browser-languagedetector"
 import {initReactI18next} from "react-i18next"
 
-i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
+i18n.use(Backend).use(initReactI18next).init({
   backend:{
     // translation file path
     loadPath: '/assets/i18n/{{ns}}/{{lng}}.json',
