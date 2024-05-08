1978d38875ec9c21d74f63f1dbf902c70a03eb62
Yusif Shikhseyidov <yusif1580@gmail.com>
2024-05-09 02:08:10 +0400

    Add an svg and configure layout & responsiveness accordingly

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -12,24 +12,30 @@ function Contact() {
     <main className="contact-page">
       <h1>{t("contact")}</h1>
       <section className="contact-details">
-        <div className="contact-phone-container">
-          <a href="tel:(+994 12) 465 73 07">
-            <div className="phone-icon-wrapper">
-              <PiPhoneIncoming size={23} />
-            </div>
-            <span>{t("contact-us")}</span>
-            <span>(+994 12) 465 73 07</span>
-          </a>
+        <div className="contact-details_contact-us-svg">
+          <img src="/contact-us-bg.svg" alt="contact-us" />
         </div>
 
-        <div className="contact-email-container">
-          <a href="mailto:info@conco.az">
-            <div className="email-icon-wrapper">
-              <TbMailOpened size={21} />
-            </div>
-            <span>{t("email-us")}</span>
-            <span>info@conco.az</span>
-          </a>
+        <div className="contact-details_contact-details-wrapper">
+          <div className="contact-phone-container">
+            <a href="tel:(+994 12) 465 73 07">
+              <div className="phone-icon-wrapper">
+                <PiPhoneIncoming size={23} />
+              </div>
+              <span>{t("contact-us")}</span>
+              <span>(+994 12) 465 73 07</span>
+            </a>
+          </div>
+
+          <div className="contact-email-container">
+            <a href="mailto:info@conco.az">
+              <div className="email-icon-wrapper">
+                <TbMailOpened size={21} />
+              </div>
+              <span>{t("email-us")}</span>
+              <span>info@conco.az</span>
+            </a>
+          </div>
         </div>
       </section>
     </main>
