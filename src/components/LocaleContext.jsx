70ff32c440647c606cbda323a2c28e65f715625f
Yusif Shikhseyidov <yusif1580@gmail.com>
2024-05-13 19:14:08 +0400

    Update files to have persist selected language feature on page refresh

--- src/components/LocaleContext.jsx
+++ src/components/LocaleContext.jsx
@@ -1,16 +1,28 @@
-import { createContext, useState, useContext } from "react"
+import { createContext, useState, useContext, useEffect } from "react"
 import i18n from "i18next"
 
 const LocaleContext = createContext()
 export const useLocale = () => useContext(LocaleContext)
 
 const LocaleProvider = ({children}) => {
-  const [locale, setLocale] = useState(i18n.language) //Default Locale
+  // initialize locale from local storage or default to 'az-Latn'
+  const [locale, setLocale] = useState(localStorage.getItem('locale') || 'az-Latn') //Default Locale
+
+  useEffect(()=>{
+    // listen for language changes
+    i18n.on('languageChanged', (lng)=>{
+      setLocale(lng)
+      // save the new locale to local storage
+      localStorage.setItem('locale', lng)
+    })
+    i18n.changeLanguage(locale)
+  }, [locale])
 
   const changeLocale = (newLocale) => {
-    setLocale(newLocale)
+    // this will trigger the 'languagechanged' event
     i18n.changeLanguage(newLocale)
   }
+
   return (
     <LocaleContext.Provider value={{locale, changeLocale}}>
       {children}
--- src/i18n.js
+++ src/i18n.js
@@ -7,7 +7,7 @@ i18n.use(Backend).use(initReactI18next).init({
     // translation file path
     loadPath: '/assets/i18n/{{ns}}/{{lng}}.json',
   },
-  fallbackLng: 'az-Latn',
+  fallbackLng: localStorage.getItem('locale') || 'az-Latn',
   debug: false,
   // we can have multiple namespaces in case we want to divide a huge translation into smaller pieces and load them on demand
   ns: ['common', 'about', 'projects', 'vacancy', 'contact', 'footer'],
